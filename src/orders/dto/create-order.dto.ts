073630ba0b6d3ce12b93b5a383511467651e9b44
kali <dgbautista@kali.muserpol.gob.bo>
2026-02-24 16:49:33 -0400

    OrderMS implementando

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,25 @@
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { OrderStatus } from "../enum/order.enum";
+
+
+export class CreateOrderDto {
+
+    @IsNumber()
+    @IsPositive()
+    totalAmount: number;
+
+    @IsNumber()
+    @IsPositive()
+    totalItems: number;
+
+    @IsEnum(OrderStatus,
+        { message: `Possible status values are: ${Object.values(OrderStatus)}`}
+    )
+    @IsOptional()
+    status: OrderStatus = OrderStatus.PENDING;
+
+    @IsBoolean()
+    @IsOptional()
+    paid: boolean = false;
+
+}
---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { ORDER_SERVICE } from 'src/config';
+
+@Controller('orders')
+export class OrdersController {
+
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly orderClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.orderClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.orderClient.send('findAllOrders', {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.orderClient.send('findOneOrder', { id: +id });
+  }
+
+}
